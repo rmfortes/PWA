a0dea3fda7c1391afdee01a3aa0de15b78b85b8b
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-07-09 16:07:08 -0400

    created navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Route, Link } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
 import ReadMe from "./Components/ReadMe/ReadMe";
 import Map from "./Components/Map/Map";
 import Camera from "./Components/Camera/Camera";
@@ -9,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <Route path="/" exact component={ReadMe} />
         <Route path="/camera" exact component={Camera} />
         <Route path="/map" exact component={Map} />
