108124fdf7feabb14d3c3bd644feef403a04b18e
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-07-09 15:04:13 -0400

    added camera and map component files

---
+++ src/Components/Camera/Camera.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Camera.css'
+
+class Camera extends Component {
+    render() {
+        return (
+            <div>
+              <p>Camera Component</p>  
+            </div>
+        );
+    }
+}
+
+export default Camera;
