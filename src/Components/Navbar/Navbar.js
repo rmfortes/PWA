d2ac284d9a6a7752d0823013db125ba9d0e52b91
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-07-09 16:13:40 -0400

    links built into navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
 
 class Navbar extends Component {
     render() {
         return (
             <div>
-               <p>navbar</p> 
+               <nav>
+                   <Link to="/"><p>ABOUT</p></Link>
+                   <Link to="/camera"><p>SNAP</p></Link>
+                   <Link to="/map"><p>MAP</p></Link>
+               </nav>
             </div>
         );
     }
