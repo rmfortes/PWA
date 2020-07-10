51e8d302be5a8195fb419a56e2c805de98cbf73e
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-07-10 16:36:21 -0400

    camera styling done

--- src/Components/Camera/Camera.js
+++ src/Components/Camera/Camera.js
@@ -1,16 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Webcam from "react-webcam";
 
-// const WebcamComponent = () => <Webcam />;
-
 class Camera extends Component {
-    render() {
-        return (
-            <div>
-              <Webcam />  
-            </div>
-        );
-    }
+  setRef = (webcam) => {
+    this.webcam = webcam;
+  };
+
+  render() {
+    const videoConstraints = {
+      width: 400,
+      height: 810,
+      facingMode: "user"
+    };
+
+    return (
+      <div>
+        <Webcam
+          className="camera"
+          audio={false}
+          height={710}
+          ref={this.setRef}
+          screenshotFormat="image/jpeg"
+          width={370}
+          videoConstraints={videoConstraints}
+        />
+        <button className="button" onClick={this.capture}>Snap Photo</button>
+      </div>
+    );
+  }
 }
 
 export default Camera;
