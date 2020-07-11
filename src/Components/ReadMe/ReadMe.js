e942075aab00795a6845e51c68640066b09bf29e
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-07-10 22:41:07 -0400

    about component renders markdown

--- src/Components/ReadMe/ReadMe.js
+++ src/Components/ReadMe/ReadMe.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ReactMarkdown from "react-markdown";
+import README from "../../../src/README.md";
 
 class ReadMe extends Component {
-    render() {
-        return (
-            <div>
-                <p>Read Me Component</p>
-            </div>
-        );
-    }
+  constructor() {
+    super();
+    this.state = { markdown: "" };
+  }
+
+  componentWillMount() {
+    fetch(README)
+      .then((res) => res.text())
+      .then((text) => this.setState({ markdown: text }));
+  }
+
+  render() {
+    const { markdown } = this.state;
+    return <ReactMarkdown source={markdown} />;
+  }
 }
 
 export default ReadMe;
