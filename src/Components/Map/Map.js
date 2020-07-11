28172e66f960bc3b24ff4f6e4243ed2e3ee04379
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-07-10 20:50:33 -0400

    map component built

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper } from "google-maps-react";
 
-class Map extends Component {
-    render() {
-        return (
-            <div>
-                <p>Map Component</p>
-            </div>
-        );
-    }
+const mapStyles = {
+    width: '100%',
+    height: '100%',
+};
+
+class MapContainer extends Component {
+  render() {
+    return (
+      <div>
+        <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176 }}
+        />
+      </div>
+    );
+  }
 }
 
-export default Map;
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyAB2Hzw2E7XcqZDbN1zJF_3GcJn-BQDsjU'
+})(MapContainer);
+
