f1a547e6b81ff88020716e661a8f262e9459b52a
REBECCA_FORTES <RMFORTES@UMICH.EDU>
2020-07-12 23:34:44 -0400

    added landing page

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Navbar from "./Components/Navbar/Navbar";
 import About from "./Components/About/About";
 import Map from "./Components/Map/Map";
 import Camera from "./Components/Camera/Camera";
+import Landing from "./Components/Landing/Landing";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Route path="/" exact component={Landing}/>
         <Route path="/about" exact component={About} />
         <Route path="/SNAP" exact component={Camera} />
         <Route path="/MAP" exact component={Map} />
---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    render() {
+        return (
+            <div className="about">
+                <h1>Welcome to Snap Map</h1>
+                <p>Click 'ABOUT', or the menu icon at the top right corner to get started.</p>
+            </div>
+        );
+    }
+}
+
+export default Landing;
